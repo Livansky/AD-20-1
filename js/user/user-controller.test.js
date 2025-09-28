a3bf959734964dc2e1520fd4ef40b1c067ef63cd
Carlos Ortiz <carlos72ortiz14@gmail.com>
2025-09-28 16:35:44 -0600

    se agrego la prueba de los usuarios

--- js/user/user-controller.test.js
+++ js/user/user-controller.test.js
@@ -16,3 +16,33 @@ test('remove user to userController', () => {
     expect(userController.users).not.toContain(user);
   });
 
+test("probar si un usuario no esta en la lista", ()=>{
+  /* controlador nuevo con la lista vacia */
+  const controller = new UserController();
+
+  const user = new User(1, 'Carlos', 'Carlos@example.com');
+
+  controller.add(user)
+
+  /* verificar que esta en la lista el usuario y es unico */
+
+  expect(controller.getUsers().length).toBe(1)
+  expect(controller.getUsers()).toContain(user)
+})
+
+
+
+test("intentar eliminar un usuario que no existe en la lista, se espera que la lista no cambie", ()=>{
+const controller2 = new UserController();
+/* anadimos dos usuarios */
+const userA = new User(1, 'Carlos', 'Carlos@gmail');
+const userB = new User(2, 'Danlya', 'Danlya@gmail');
+controller2.add(userA)
+controller2.add(userB)
+const userC= new User(3, 'Javier','Javer@gmail');
+controller2.remove(userC);
+expect(controller2.getUsers().length).toBe(2)
+expect(controller2.getUsers()).toContain(userA)
+expect(controller2.getUsers()).toContain(userB)
+})
+
